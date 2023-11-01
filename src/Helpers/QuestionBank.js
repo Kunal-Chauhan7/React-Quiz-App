be8c4257d7f19ea2ab439f825e228fd5a1050e79
Kunal Chauhan <kunalchauhanlion@gmail.com>
2023-11-01 17:21:03 +0530

    done making the quiz app

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+        prompt: "What is 1+1?",
+        optionA: "2",
+        optionB: "4",
+        optionC: "6",
+        optionD: "8",
+        answer: "A",
+    },
+    {
+        prompt: "What is 1+2?",
+        optionA: "2",
+        optionB: "3",
+        optionC: "6",
+        optionD: "8",
+        answer: "B",
+    },
+    {
+        prompt: "What is 2+2",
+        optionA: "2",
+        optionB: "4",
+        optionC: "6",
+        optionD: "8",
+        answer: "B",
+    },
+];
